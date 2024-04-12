e27e4290630002a15f9ff0fe382314969948db40
Feed-dev <federico.lubbert@gmail.com>
2024-04-12 09:44:11 +0200

    done

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def index():
     return render_template('index.html', posts=posts)
 
 
-@app.route('/post/<int:post_id>')
+@app.route('/<int:post_id>')
 def post(post_id):
     post = next((post for post in posts if post["id"] == post_id), None)
     if post is None:
